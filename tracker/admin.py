9e2d481dc2b7492a5f060d28753f53087c20136e
varshinivr <93777160+varshinivr@users.noreply.github.com>
2023-05-18 11:35:46 +0530

    Add files via upload

---
+++ tracker/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import User, Announcements, Stops, Route, Bus, BusCoordinates, Bookings
+# Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Announcements)
+admin.site.register(Stops)
+admin.site.register(Route)
+admin.site.register(Bus)
+admin.site.register(BusCoordinates)
+admin.site.register(Bookings)
---
+++ tracker/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('login', views.login_view, name="login"),
+    path('register', views.register, name="register"),
+    path('logout', views.logout_view, name="logout" ),
+    path('announcements', views.announcements, name="announcements"),
+    path('tracker', views.tracker, name="tracker"),
+    path('bookings', views.bookings, name="bookings"),
+    path('addwbus', views.addwbus, name="addwbus"),
+    path('addannouncement', views.addannouncement, name="addannouncement"),
+    path('routes', views.routes, name="routes"),
+    path('profile/<str:code>', views.profile, name="profile"),
+    path('viewbookings/<int:code>', views.viewbookings, name='viewbookings'),
+    path('deleteannouncement/<int:code>', views.deleteannouncement, name="deleteannouncement"),
+    path('deletebus/<int:code>', views.deletebus, name="deletebus"),
+    path('bookbus/<int:code>/<int:user>', views.bookbus, name="bookbus"),
+    path('cancelbus/<int:code>/<int:user>', views.cancelbus, name="cancelbus"),
+    
+    # API Routes
+    path('route/<int:code>', views.route, name="route"),
+    path('routestops/<int:code>', views.routestops, name="routestops"),
+    path('getlocation/<int:code>', views.getlocation, name="getlocation"),
+    path('setlocation/<int:code>', views.setlocation, name="setlocation"),
+
+]
